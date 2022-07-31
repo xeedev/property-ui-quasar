577d285b1ebf5cc58d9ce89eb5e9b773327ad845
xeemax <xeemax2.0@gmail.com>
2022-07-31 14:21:03 +0500

    setting up

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,128 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="bg-green-5">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Pride Properties </q-toolbar-title>
+        <q-space></q-space>
+        <q-btn class="bg-yellow-5" label="Logout" @click="logout">
+          <!-- <q-menu>
+            <q-list style="min-width: 100px">
+              <q-item clickable v-close-popup @click="$router.push('/login')">
+                <q-item-section>Logout</q-item-section>
+              </q-item>
+              <q-separator />
+            </q-list>
+          </q-menu> -->
+        </q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header>
+          <q-img
+            src="download.jfif"
+            spinner-color="white"
+            @click="$router.push('/dashboard')"
+            fit="contain"
+            height="80px"
+            class="q-ma-sm cursor-pointer"
+            style="max-width: 150px"
+          />
+        </q-item-label>
+        <template v-for="(essentialLink,index) in essentialLinks" :key="index">
+          <q-item clickable tag="a" @click="$router.push(essentialLink.link)">
+            <q-item-section avatar>
+              <q-icon :name="essentialLink.icon" />
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>{{essentialLink.title}}</q-item-label>
+              <q-item-label caption>{{essentialLink.caption}}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Api from 'src/services/api';
+import { useRouter } from 'vue-router'
+// import EssentialLink from 'components/EssentialLink.vue';
+const linksList = [
+  {
+    title: 'Dashboard',
+    caption: 'Statistics',
+    icon: 'dashboard',
+    link: 'dashboard',
+  },
+  {
+    title: 'Users',
+    caption: 'Manage Users',
+    icon: 'account_circle',
+    link: 'users',
+  },
+  {
+    title: 'Deals',
+    caption: 'Track Deals',
+    icon: 'trending_up',
+    link: 'deals',
+  },
+  {
+    title: 'Societies',
+    caption: 'Manage Societies',
+    icon: 'category',
+    link: 'societies',
+  },
+  {
+    title: 'Media',
+    caption: 'Manage Gallery',
+    icon: 'image',
+    link: 'gallery',
+  },
+];
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    // EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const router = useRouter()
+    async function logout(){
+      await Api.post('logout');
+      localStorage.removeItem('token')
+      localStorage.removeItem('token_check')
+      router.push({path:'login'})
+    }
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      logout,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,108 @@
+<template>
+  <q-page class="row">
+    <div class="q-pa-md row items-start q-gutter-md justify-center">
+      <q-card class="my-card bg-green-5">
+        <q-card-section class="q-ma-xl">
+          <div class="row justify-center items-center text-white text-center">
+            Total Customers
+            <br>
+            {{ statistics.customers }}
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card bg-amber-5 cursor-pointer" @click="$router.push('orders')">
+        <q-card-section class="q-ma-xl">
+          <div class="row justify-center items-center text-white text-center">
+            All Deals
+            <br>
+            {{ statistics.orders }}
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card bg-red-5 cursor-pointer" @click="$router.push('orders')">
+        <q-card-section class="q-ma-xl">
+          <div class="row justify-center items-center text-white text-center">
+            Pending Deals
+            <br>
+            {{ statistics.pendingOrders }}
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card bg-blue-5 cursor-pointer" @click="$router.push('orders')">
+        <q-card-section class="q-ma-xl">
+          <div class="row justify-center items-center text-white text-center">
+            Today's Deals
+            <br>
+            {{ statistics.todaysOrders }}
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card bg-dark cursor-pointer" @click="$router.push('products')">
+        <q-card-section class="q-ma-xl">
+          <div class="row justify-center items-center text-white text-center">
+            Total Societies
+            <br>
+            {{ statistics.products }}
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card bg-grey cursor-pointer" @click="$router.push('categories')">
+        <q-card-section class="q-ma-xl">
+          <div class="row justify-center items-center text-white text-center">
+            Total Categories
+            <br>
+            {{ statistics.categories }}
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Todo, Meta } from 'components/models';
+import Api from "src/services/api";
+import { defineComponent, ref, onMounted } from 'vue';
+
+export default defineComponent({
+  name: 'DashboardPage',
+  setup() {
+    onMounted( async () =>{
+      let res = await Api.getList('statistics');
+      statistics.value = res.data
+    });
+    const todos = ref<Todo[]>([
+      {
+        id: 1,
+        content: 'ct1',
+      },
+      {
+        id: 2,
+        content: 'ct2',
+      },
+      {
+        id: 3,
+        content: 'ct3',
+      },
+      {
+        id: 4,
+        content: 'ct4',
+      },
+      {
+        id: 5,
+        content: 'ct5',
+      },
+    ]);
+    const meta = ref<Meta>({
+      totalCount: 1200,
+    });
+    const statistics = ref('');
+    return { todos, meta,statistics };
+  },
+});
+</script>
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
