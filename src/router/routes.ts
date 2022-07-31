c0686ecfc2590e8aadbf525fece665cf0262c5e7
xeemax <xeemax2.0@gmail.com>
2022-08-01 02:11:15 +0500

    configuration changes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,75 @@
 import { RouteRecordRaw } from 'vue-router';
+import Api from 'src/services/api';
 
 const routes: RouteRecordRaw[] = [
+
+  {
+    path: '/login',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('src/pages/LoginPage.vue') },
+    ],
+  },
   {
     path: '/',
     component: () => import('layouts/DashboardLayout.vue'),
-    children: [{ path: '', component: () => import('pages/DashboardPage.vue') }],
+    children: [
+      { path: '', component: () => import('src/pages/DashboardPage.vue') },
+    ],
+    beforeEnter: async (to, from, next) =>
+    {
+      if (await IsAuthenticated()) next();
+      else next('/login');
+    }
   },
+  // {
+  //   path: '/products',
+  //   component: () => import('layouts/DashboardLayout.vue'),
+  //   children: [
+  //     { path: '', component: () => import('src/pages/DisplayProductsPage.vue') },
+  //   ],
+  //   beforeEnter: async (to, from, next) =>
+  //   {
+  //     if (await IsAuthenticated()) next();
+  //     else next('/login');
+  //   }
+  // },
+  // {
+  //   path: '/categories',
+  //   component: () => import('layouts/DashboardLayout.vue'),
+  //   children: [
+  //     { path: '', component: () => import('src/pages/ManageCategoriesPage.vue') },
+  //   ],
+  //   beforeEnter: async (to, from, next) =>
+  //   {
+  //     if (await IsAuthenticated()) next();
+  //     else next('/login');
+  //   }
+  // },
+  // {
+  //   path: '/orders',
+  //   component: () => import('layouts/DashboardLayout.vue'),
+  //   children: [
+  //     { path: '', component: () => import('src/pages/ManageOrdersPage.vue') },
+  //   ],
+  //   beforeEnter: async (to, from, next) =>
+  //   {
+  //     if (await IsAuthenticated()) next();
+  //     else next('/login');
+  //   }
+  // },
+  // {
+  //   path: '/gallery',
+  //   component: () => import('layouts/DashboardLayout.vue'),
+  //   children: [
+  //     { path: '', component: () => import('src/pages/ManageGalleryPage.vue') },
+  //   ],
+  //   beforeEnter: async (to, from, next) =>
+  //   {
+  //     if (await IsAuthenticated()) next();
+  //     else next('/login');
+  //   }
+  // },
 
   // Always leave this as last one,
   // but you can also remove it
@@ -14,5 +78,9 @@ const routes: RouteRecordRaw[] = [
     component: () => import('pages/ErrorNotFound.vue'),
   },
 ];
+async function IsAuthenticated(){
+  const res =   await Api.getList( 'validateToken');
+  return !!res?.data?.authenticated;
+}
 
 export default routes;
