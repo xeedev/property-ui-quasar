b49f042bbdf39ac42d873af9a18752ee28eee7a9
xeemax <xeemax2.0@gmail.com>
2022-08-01 02:12:47 +0500

    added main layout and login oage

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="bg-green-5">
+      <q-toolbar>
+<!--        <q-btn-->
+<!--          flat-->
+<!--          dense-->
+<!--          round-->
+<!--          icon="menu"-->
+<!--          aria-label="Menu"-->
+<!--          @click="toggleLeftDrawer"-->
+<!--        />-->
+
+<!--        <q-toolbar-title> Pride Properties </q-toolbar-title>-->
+<!--        <q-space></q-space>-->
+<!--        <q-btn class="bg-yellow-5" label="Logout" @click="logout">-->
+          <!-- <q-menu>
+            <q-list style="min-width: 100px">
+              <q-item clickable v-close-popup @click="$router.push('/login')">
+                <q-item-section>Logout</q-item-section>
+              </q-item>
+              <q-separator />
+            </q-list>
+          </q-menu> -->
+<!--        </q-btn>-->
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent} from 'vue';
+export default defineComponent({
+  name: 'MainLayout',
+});
+</script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="q-py-xl">
+    <q-card
+      :style="$q.screen.sm || $q.screen.xs ? '' : { width: '350px' }"
+      class="ma-auto-0 q-py-xl text-center"
+      :class="$q.screen.sm || $q.screen.xs ? 'flat' : ''"
+    >
+      <q-img
+        src="download.jfif"
+        spinner-color="white"
+        @click="$router.push('/')"
+        fit="contain"
+        width="100px"
+        class="q-ma-sm cursor-pointer"
+      />
+      <q-card-section>
+        <q-form @submit="onSubmit" class="q-gutter-md">
+          <q-input
+            outlined
+            dense
+            type="email"
+            v-model="Email"
+            :error-message="error"
+            :error="!isValid"
+            label="Your Email *"
+            hint="Email"
+            lazy-rules
+            :rules="[
+              (val) => (val && val.length > 0) || 'Please enter your email',
+            ]"
+          />
+
+          <q-input
+            outlined
+            dense
+            type="password"
+            v-model="Password"
+            label="Password"
+            hint="Password"
+            lazy-rules
+            :rules="[
+              (val) => (val && val.length > 0) || 'Password is required',
+            ]"
+          />
+
+          <div class="flex justify-center">
+            <q-btn label="Login" class="q-px-xl bg-green-5 text-white" style="width: 60%" type="submit" />
+<!--            <span class="text-blue cursor-pointer">Forgot Password?</span>-->
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+<script lang="ts">
+import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
+import Api from '../services/api/index'
+import {useAuthStore} from 'stores/useAuth'
+import { ref, onMounted } from 'vue';
+
+export default {
+  setup() {
+    onMounted(async () => {
+       await Api.getList( 'validateToken').then((response) => {
+        if (response?.data?.authenticated && localStorage.getItem('token_check') === process.env.ADMIN){
+          router.push('/dashboard');
+        }else if(response?.data?.authenticated && localStorage.getItem('token_check') === process.env.USER){
+          router.push('/');
+        }
+      });
+    })
+    const $q = useQuasar();
+    const router = useRouter();
+    const store = useAuthStore();
+
+    const Email = ref(null);
+    const Password = ref(null);
+    const error = ref('');
+    const isValid = ref(true);
+    return {
+      Email,
+      Password,
+      error,
+      isValid,
+
+
+
+      async onSubmit() {
+        $q.loading.show();
+        let res = await Api.login({
+          email : Email.value,
+          password : Password.value
+        });
+        if (res?.data?.error){
+          isValid.value = false;
+          error.value = res.data.error
+        }else if(res?.data?.data?.token && res?.data?.data?.me === 1){
+          localStorage.setItem('token', res?.data?.data?.token);
+          localStorage.setItem('token_check', process.env.ADMIN!);
+
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Success',
+          });
+          router.push('/dashboard');
+        }else if(res?.data?.data?.token && res?.data?.data?.me === 0){
+          store.changeAuthStatus(true)
+          localStorage.setItem('token', res?.data?.data?.token);
+          localStorage.setItem('token_check', process.env.USER!);
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Success',
+          });
+          router.push('/');
+        }else{
+          isValid.value = false;
+          error.value = 'Something went wrong please try again'
+        }
+        $q.loading.hide();
+      },
+    };
+  },
+};
+</script>
+<style>
+.ma-auto-0 {
+  margin: 0 auto;
+}
+</style>
